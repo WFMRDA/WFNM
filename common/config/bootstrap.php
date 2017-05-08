c697b2426018863ec7e5ee026afb541d975aa35a
Pyronerd62 <rgoolsby@firenet.gov>
2017-05-08 14:38:51 -0400

    Moved Media Alias to Cloudfront

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,5 +4,5 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 
-Yii::setAlias('@media','//media.wildfiresnearme.wfmrda.com/img');
+Yii::setAlias('@media','//assets.wildfiresnearme.wfmrda.com/img');
 Yii::setAlias('@assets','//assets.wildfiresnearme.wfmrda.com');
